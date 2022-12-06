7305462ed62fbb7773c752799a47b9b4a737b87b
Gabriel-424 <73781875+Gabriel-424@users.noreply.github.com>
2022-12-06 11:08:16 +0100

    V1.2
    sdq

--- main.m
+++ main.m
@@ -20,9 +20,25 @@ wopt = [1, 1/2, 1/4].';
 
 
 figure(2);
-subplot(411), plot(e);
-subplot(412), plot(w.');
-subplot(413), plot(abs(w-wopt).')
-subplot(414), plot(abs(w-wopt).^2.')
+subplot(411),
+plot(e);
+title("Signal d'erreur e_n");
+legend("e_n");
+
+subplot(412),
+plot(w.');
+title("Evolution des coefficients du filtre w_n au cours du temps");
+legend("w_1","w_2","w_3");
+
+subplot(413), 
+plot(abs(w-wopt).');
+title("Evolution de l’erreur sur les coefficients du filtre |w_n-w_{opt}|");
+legend("|w_1-w_{opt}|","|w_2-w_{opt}|","|w_3-w_{opt}|");
+
+subplot(414), 
+plot(abs(w-wopt).^2.')
+title("Evolution de la norme 2 au carré de l’erreur sur le filtre")
+legend("|w_1-w_{opt}|^2","|w_2-w_{opt}|^2","|w_3-w_{opt}|^2");
+
 %disp(w)
 soundsc(e, Fe);
