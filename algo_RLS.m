0be56ecc9eeaeccedee1b92dde3f105cf70b42a2
pyPitiot <37814471+BOB-Laiponge@users.noreply.github.com>
2022-12-08 08:53:56 +0100

    V1.4.1
    rajout de comms

--- algo_RLS.m
+++ algo_RLS.m
@@ -1,8 +1,13 @@
 function [e,w] = algo_RLS(x,d,P,lambda, delta)
 
-
-
-for 
+N = length(x);
+w = zeros(P,N+1);
+e = zeros(N,1);
+y = zeros(N,1);
+X = zeros(P,1);
+K = zeros(N);
+for n = 2:N
+    
 end
 
 end
